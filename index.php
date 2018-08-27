febf576f0ceb228b9226b53cfac24b484931895c
root <root@Debian-94-stretch-64-LAMP144.76.201.133>
2018-08-27 13:23:32 +0300

    Поправил комментарии, и ссылку

--- index.php
+++ index.php
@@ -4,7 +4,6 @@ include('simple_html_dom.php');
 
 // пример запуска ( спарсит все марки с сайта rusmarka.ru, страницу 1, года 2017, в раздел с id 1885 )
 // parser.html?year=2017&page=1&parent=1885&PageSpeed=off
-// https://www.stamp-collection.ru/parser.html?year=2017&page=1&parent=1885&PageSpeed=off
 
 // page = 0,1,2,3,4...
 // year = 2017,2018...
